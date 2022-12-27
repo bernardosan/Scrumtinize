355285fa172c61e625bb8c5f740f71bfb63dc720
Bernardo Santiago de Souza <108026375+bernardosan@users.noreply.github.com>
2022-12-26 23:54:25 -0300

    Implemented login with google functionality

--- app/src/main/java/com/example/trellocloneapp/activities/SignInActivity.kt
+++ app/src/main/java/com/example/trellocloneapp/activities/SignInActivity.kt
@@ -4,16 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.trellocloneapp.R
 import com.example.trellocloneapp.databinding.ActivitySignInBinding
 import com.example.trellocloneapp.firebase.FirestoreClass
 import com.example.trellocloneapp.utils.Constants
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class SignInActivity : BaseActivity() {
 
     private var binding: ActivitySignInBinding? = null
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +32,15 @@ class SignInActivity : BaseActivity() {
         setSupportActionBar(binding?.toolbarSignIn)
 
         auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.oauth_google_cloud_key)) // OAuth Web Api Key do Google Cloud
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
 
         if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -49,6 +68,10 @@ class SignInActivity : BaseActivity() {
         binding?.llSignup?.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
             finish()
+        }
+
+        binding?.btnGoogle?.setOnClickListener {
+            signinGoogle()
         }
 
 
@@ -104,4 +127,42 @@ class SignInActivity : BaseActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
+
+    private fun signinGoogle() {
+
+        val intent = googleSignInClient.signInIntent
+        openActivity.launch(intent)
+    }
+
+    var openActivity = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        result: ActivityResult ->
+
+        if(result.resultCode == RESULT_OK){
+            val int = result.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(int)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                showProgressDialog(getString(R.string.please_wait))
+                loginWithGoogle(account.idToken)
+            } catch (e: ApiException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun loginWithGoogle(token: String?) {
+        val credentials = GoogleAuthProvider.getCredential(token, null)
+        auth.signInWithCredential(credentials).addOnCompleteListener {
+            task : Task<AuthResult> ->
+            if (task.isSuccessful) {
+                FirestoreClass().updateUserData(this)
+            } else {
+                showErrorSnackBar("${task.exception!!.message}")
+            }
+        }
+    }
+
+
 }
